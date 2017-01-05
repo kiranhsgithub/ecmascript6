459b86be134ac7c52defb37a4a631833c4034eed
kiranhsgithub <hskiran.bms@gmail.com>
2017-01-05 16:43:07 -0500

    generators continued

--- Ch02/02_02/start/generators.js
+++ Ch02/02_02/start/generators.js
@@ -15,3 +15,22 @@ printYield(action.next());
 printYield(action.next());
 printYield(action.next());
 printYield(action.next());
+
+function* eachItem(arr) {
+    for(var i=0; i< arr.length; i++) {
+        yield arr[i];
+    }
+}
+
+var vowels = eachItem(['a', 'e', 'i', 'o', 'u']);
+
+var abcs = setInterval(function() {
+    var vowel = vowels.next();
+    if(vowel.done) {
+        clearInterval(abcs);
+        console.log('Now I know my vowels');
+    } else {
+      console.log(vowel.value);  
+    }
+}, 1000);
+
