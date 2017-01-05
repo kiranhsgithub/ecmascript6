5da9fc598f57949afbd31cea384fc857d8452cd1
kiranhsgithub <hskiran.bms@gmail.com>
2017-01-05 16:02:45 -0500

    arrow functions

--- Ch02/02_02/start/arrow.js
+++ Ch02/02_02/start/arrow.js
@@ -14,13 +14,21 @@ var person = {
     }, 
     passions: ['art', 'history'],
     printPassions: function() {
-        this.passions.forEach(function(action) {
-           var str = this.first + " is passionate about " + action;
+        this.passions.forEach(function(passion) {
+           var str = this.first + " is passionate about " + passion;
             console.log(str);
         }.bind(this));
+    },
+    cars: ['BMW', 'Audi'],
+    printCars() {
+        this.cars.forEach( car => {
+           var str = this.first + " owns a " + car;
+           console.log(str);
+        });
     }
     
 }
 
 person.printActions();
 person.printPassions();
+person.printCars();
